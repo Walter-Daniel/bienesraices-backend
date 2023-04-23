dc339c040addc61d3897f557536097ce928aa9dc
Walter <walterdcarrizo.19@gmail.com>
2023-04-23 10:47:20 -0300

    instalación de express-validator y uso de check en rutas

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors =  require('cors')
 //Crear el server
 const app = express();
 
-//Habilitar lectura json
+//Habilitar la lectura y parseo del body
 app.use(express.json());
 
 //Habilitar lectura de datos formulario
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,36 @@
 const express = require('express');
-const { login, register } = require('../controllers/user.controller');
 const router = express.Router();
+const { check } = require('express-validator');
+const { login, register } = require('../controllers/user.controller');
 
-router.post('/', login);
-router.post('/register', register);
+router.post('/', 
+[
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'La contraseña es obligatoria')
+                .isStrongPassword({
+                        minLength: 6,
+                        maxLength: 12, 
+                        minLowercase: 1, 
+                        minUppercase: 1, 
+                        minSymbols: 1
+                        }).withMessage('La contraseña debe tener al menos: una letra minúscula, una mayúscula, un caracter especial y una longitud entre 6 y 12 caracteres'),
+],
+     login);
+router.post('/register',
+[
+        check('name', 'El campo Nombre es obligatorio')
+                .not().isEmpty()
+                .matches(/^[a-zA-ZÀ-ÿ\s]{1,40}$/).withMessage('El campo debe contener solamente letras y espacios.')
+                .isLength({ min: 2, max: 30}).withMessage('Debe tener una longitud entre 2 y 12 caracteres'),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'La contraseña es obligatoria')
+                .isStrongPassword({
+                        minLength: 6,
+                        maxLength: 12, 
+                        minLowercase: 1, 
+                        minUppercase: 1, 
+                        minSymbols: 1
+                }).withMessage('La contraseña debe tener al menos: una letra minúscula, una mayúscula, un caracter especial y una longitud entre 6 y 12 caracteres'),
+], register);
 
 module.exports = router;
